a2ba42d515a04f6ef08a35fdf712e7cfeb7c41d6
Haim Guel Quiroz <hgq_2000@hotmail.com>
2022-04-27 17:46:23 -0500

    Add Explanation

--- test/reader.test.js
+++ test/reader.test.js
@@ -5,9 +5,8 @@ test('1) Test reader class', () => {
     expect(explorers).toBeDefined();
 })
 
-test('2) Test Explorer quantity of explorers by mission', () => {
+test('2) Get the quantity of explorers names in node', () => {
     const explorers = Reader.readJsonFile('explorers.json');
-    // Part 2: Get the quantity of explorers names in node
     const quantity = ExplorersService.getExplorersAmountByMission(explorers,"node");
     expect(quantity).toBeGreaterThan(0);
 })
