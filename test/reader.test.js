d0e5c9acf844b321a7478c5a049bc76c4f915aae
Haim Guel Quiroz <hgq_2000@hotmail.com>
2022-04-27 14:10:58 -0500

    Add reader.test.js

---
+++ test/reader.test.js
@@ -0,0 +1,5 @@
+const Reader = require('./../lib/utils/Reader')
+test('1) Test reader class', () => {
+    const explorers = Reader.readJsonFile('./../explorers.json');
+    expect(explorers).toBeDefined();
+})
