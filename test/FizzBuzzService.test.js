77f3a8953f80c8fccbe232c62b1373a91b1aed20
Haim Guel Quiroz <hgq_2000@hotmail.com>
2022-04-29 15:53:52 -0500

    Fix changes

---
+++ test/FizzBuzzService.test.js
@@ -0,0 +1,10 @@
+const FizzBuzzService = require('../lib/services/FizzbuzzService');
+
+describe("Check FizzBuzz functionality", () => {
+    test('1) FizzBuzz testing', () => {
+        const explorers = [{name: "explorer1", score: 3}];    
+        const explorerValidation = FizzBuzzService.applyValidationinExplorers(explorers);
+        expect(explorerValidation).toEqual([{name: "explorer1", trick: "FIZZ", score: 3}]);
+    });
+});
+
--- test/reader.test.js
+++ test/reader.test.js
@@ -1,37 +1,5 @@
-const Reader = require('./../lib/utils/Reader')
-const ExplorersService = require('./../lib/services/ExplorerServices')
-const FizzBuzzService = require('./../lib/services/FizzbuzzService')
+const Reader = require('../lib/utils/Reader')
 test('1) Test reader class', () => {
-    const explorers = Reader.readJsonFile('explorers.json');
+    const explorers = Reader.readJsonFile('./test/data/explorers.test.json');
     expect(explorers).toBeDefined();
-})
-
-test('2) Get the quantity of explorers names in node', () => {
-    const explorers = Reader.readJsonFile('explorers.json');
-    const quantity = ExplorersService.getExplorersAmountByMission(explorers,"node");
-    expect(quantity).toBeGreaterThan(0);
-})
-
-test('3) Get the explorers usernames in Node', () => {
-    const explorers = Reader.readJsonFile('explorers.json');
-    const usernamesInNode = ExplorersService.getExplorersUserNamesByMission(explorers,"node");
-    expect(usernamesInNode).toBeTruthy(usernamesInNode.isArray);
-})
-
-test('4) FizzBuzz testing', () => {
-    const explorers = Reader.readJsonFile('explorers.json');
-    const explorerValidation = FizzBuzzService.applyValidationinExplorers(explorers);
-    const filterFizzExplorers = explorerValidation.filter(explorer => explorer % 3 === 0);
-    const expectFizz = filterFizzExplorers.some((explorer) => explorer.score != "FIZZ");
-    expect(expectFizz).toBeFalsy();
-})
-
-test('5) Filtering by score', () => {
-    const explorers = Reader.readJsonFile('explorers.json');
-    const explorerValidation = FizzBuzzService.applyValidationinExplorers(explorers);
-    const filterScoreExplorers = explorerValidation.filter(explorer => explorer % 3 != 0);
-    const expectAnynumber = filterScoreExplorers.some((explorer) => !explorer.score.isInteger);
-
-    expect(expectAnynumber).toBeTruthy();
-
 })
